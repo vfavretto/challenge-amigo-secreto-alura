91766cdf15f5e098f5d7c426282b2101738e01ee
vfavretto <vf.favretto@gmail.com>
2025-03-05 19:19:08 -0300

    feat: Add friends base

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nome = inputAmigo.trim();
+
+    if (nome === ''){
+        alert('Por favor, digite um nome válido');
+        return;
+    }
+
+    amigos.push(nome);
+
+    atualizarListaAmigos();
+
+    inputAmigo.value = '';
+    inputAmigo.focus();
+
+    limparResultado();
+}
+
+function atualizarListaAmigos() {
+    
+}
