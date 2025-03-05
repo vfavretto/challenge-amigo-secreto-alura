86d376a64fd97380b39c19d96a5f68aaca76623d
vfavretto <vf.favretto@gmail.com>
2025-03-05 20:10:54 -0300

    feat: finish JS & fix: Add Friend function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let amigos = [];
 
 function adicionarAmigo() {
     const inputAmigo = document.getElementById('amigo');
-    const nome = inputAmigo.trim();
+    const nome = inputAmigo.value.trim();
 
     if (nome === ''){
         alert('Por favor, digite um nome válido');
@@ -43,4 +43,40 @@ function removerAmigo(index) {
     amigos.splice(index, 1);
     atualizarListaAmigos();
     limparResultado();
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Por favor, adicione pelo menos um amigos antes de sortear');
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    exibirResultado(amigoSorteado);
+}
+
+function exibirResultado(amigo) {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    const li = document.createElement('li');
+    li.textContent = `🎉O amigo secreto é: ${amigo}🎉`;
+    resultado.appendChild(li);
+}
+
+
+function limparResultado() {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+}
+
+document.getElementById('amigo').addEventListener('keypess', function(event) {
+    if (event.key === 'Enter') {
+        adicionarAmigo();
+    }
+});
+
+window.onload = function() {
+    atualizarListaAmigos();
 }
