5e57323f9c7b58622cb46df01ceaefba7f3a4078
vfavretto <vf.favretto@gmail.com>
2025-03-05 19:24:44 -0300

    feat: att and remove friends

--- app.js
+++ app.js
@@ -20,5 +20,27 @@ function adicionarAmigo() {
 }
 
 function atualizarListaAmigos() {
-    
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    amigos.forEach((amigos, index) => {
+        const li = document.createElement('li');
+        li.textContent = amigos;
+
+        const botaoRemover = document.createElement('button');
+        botaoRemover.textContent = 'X';
+        botaoRemover.classList.add('botao-remover');
+        botaoRemover.onclick = () => {
+            removerAmigo(index);
+        };
+
+        li.appendChild(botaoRemover);
+        listaAmigos.appendChild(li);
+    });
+}
+
+function removerAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarListaAmigos();
+    limparResultado();
 }
